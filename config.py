7e8c806d321053bdbd4a1d45ea8a61aef0320251
Jan Sparnaaij <jcmsparnaaij@gmail.com>
2025-01-11 11:29:12 +0100

    prepare heroku deployment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     """Base configuration class."""
-    SECRET_KEY = os.environ.get('SECRET_KEY', '6a1fdb15c0b833d5a48a1a1f45627c93b06fdef52a3c2760f234fadb70d5e3d8')  # Use env variable or fallback
+    SECRET_KEY = os.environ.get('SECRET_KEY')  # Use env variable
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'SimpleCache'
