2dfae56ee89bc3f8d5362ba043a9791c6b5dfd29
Jan Sparnaaij <jcmsparnaaij@gmail.com>
2025-01-11 18:34:30 +0100

    review playwright arguments

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
+import os
 from app import create_app
 
+# Load environment variables from the .env file
+load_dotenv()
+
+# Create the Flask application
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
