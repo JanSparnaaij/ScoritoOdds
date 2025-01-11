f6021f6f20c5e25abc6a023ee44b38b645d0adbf
Jan Sparnaaij <jcmsparnaaij@gmail.com>
2025-01-11 01:08:03 +0100

    created login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,28 @@
-# app/__init__.py
+from flask import Flask
+from flask_caching import Cache
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+# Initialize extensions
+db = SQLAlchemy()
+migrate = Migrate()
+cache = Cache()
+
+def create_app():
+    """Application Factory"""
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+    cache.init_app(app)
+
+    # Register blueprints
+    from app.routes import main_bp
+    from app.auth import auth_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
