cce53629ece8d8255d60c12ac87c440b3e24be95
Jan Sparnaaij <jcmsparnaaij@gmail.com>
2025-01-13 19:52:00 +0100

    implementation redis and db

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, current_app
-from app.football_fetcher import fetch_all_matches_async
-from app.tennis_fetcher import fetch_combined_tennis_data
 from app.player_ratings import PLAYER_RATINGS
 from app import cache, db
-from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User
 from app.tasks import fetch_matches_in_background, fetch_tennis_matches_in_background
+from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
 # Blueprint for main routes
@@ -17,22 +15,12 @@ LEAGUES = {
     "eredivisie": "https://www.oddsportal.com/football/netherlands/eredivisie/",
     "eerste_divisie": "https://www.oddsportal.com/football/netherlands/eerste-divisie/",
     "premier_league": "https://www.oddsportal.com/football/england/premier-league/",
-    "jupiler_pro_league": "https://www.oddsportal.com/football/belgium/jupiler-pro-league/",
-    "bundesliga": "https://www.oddsportal.com/football/germany/bundesliga/",
-    "serie_a": "https://www.oddsportal.com/football/italy/serie-a/",
-    "la_liga": "https://www.oddsportal.com/football/spain/laliga/",
-    "champions_league": "https://www.oddsportal.com/football/europe/champions-league/",
-    "europa_league": "https://www.oddsportal.com/football/europe/europa-league/",
 }
 
 TENNIS_LEAGUES = {
     "atp_australian_open": {
         "matches": "https://www.oddsportal.com/tennis/australia/atp-australian-open/",
         "rounds": "https://www.oddsportal.com/tennis/australia/atp-australian-open/standings/",
-    },
-    "wta_australian_open": {
-        "matches": "https://www.oddsportal.com/tennis/australia/wta-australian-open/",
-        "rounds": "https://www.oddsportal.com/tennis/australia/wta-australian-open/standings/",
     },
 }
 
@@ -91,7 +79,6 @@ def login():
             session.clear()
             session['user_id'] = user.id
             flash('Logged in successfully!', 'success')
-            current_app.logger.info(f"User {username} logged in.")
             return redirect(url_for('main.home'))
         else:
             flash('Invalid credentials. Please try again.', 'danger')
@@ -107,7 +94,7 @@ def signup():
         password = request.form['password']
 
         if len(password) < 8 or not re.search(r"\d", password) or not re.search(r"[A-Z]", password):
-            flash("Password must be at least 8 characters long, contain a number, and an uppercase letter.", "danger")
+            flash("Password must be at least 8 characters long, contain a number, and an uppercase letter.", 'danger')
             return redirect(url_for('auth.signup'))
 
         if User.query.filter_by(username=username).first():
@@ -127,5 +114,5 @@ def signup():
 def logout():
     """Logout page"""
     session.clear()
-    flash('Logged out successfully!', 'success')
+    flash('Logged out successfully.', 'success')
     return redirect(url_for('main.home'))
