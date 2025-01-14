3787933d52555efbdacf1eb7878b7b702b9d3f19
Jan Sparnaaij <jcmsparnaaij@gmail.com>
2025-01-14 12:35:15 +0100

    config problem

--- app/tasks.py
+++ app/tasks.py
@@ -1,48 +1,49 @@
 from app.celery_worker import celery
 from app.tennis_fetcher import fetch_combined_tennis_data
 from app.football_fetcher import fetch_all_matches_async
-from app import cache
-import asyncio
+from app import cache, create_app
 import logging
+import sys
+import os
+
+# path for config file
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+print("Current PYTHONPATH:", sys.path)
+
 
 logger = logging.getLogger(__name__)
 
+app = create_app()  # Create the Flask app instance
+
+
 @celery.task(name="app.tasks.fetch_tennis_matches_in_background")
 def fetch_tennis_matches_in_background(league):
-    """
-    Fetch tennis matches in the background and cache them.
-    """
-    from app.routes import TENNIS_LEAGUES  # Local import to avoid circular dependencies
-    league_urls = TENNIS_LEAGUES.get(league, {})
-    matches_url = league_urls.get("matches")
-    rounds_url = league_urls.get("rounds")
+    with app.app_context():
+        from app.routes import TENNIS_LEAGUES
 
-    logger.info(f"Fetching tennis matches for league: {league} with matches URL: {matches_url} and rounds URL: {rounds_url}")
-    try:
-        matches = fetch_combined_tennis_data(matches_url, rounds_url)
-        logger.info(f"Fetched tennis matches: {matches}")
-        cache.set(f"tennis_matches_{league}", matches, timeout=3600)
-        logger.info(f"Tennis data for {league} successfully cached.")
-        return matches
-    except Exception as e:
-        logger.error(f"Error fetching tennis data for {league}: {e}")
-        return None
+        league_urls = TENNIS_LEAGUES.get(league, {})
+        matches_url = league_urls.get("matches")
+        rounds_url = league_urls.get("rounds")
+
+        try:
+            matches = fetch_combined_tennis_data(matches_url, rounds_url)
+            cache.set(f"tennis_matches_{league}", matches, timeout=3600)
+            logger.info(f"Tennis data for {league} successfully cached.")
+        except Exception as e:
+            logger.error(f"Error fetching tennis data for {league}: {e}")
 
 
 @celery.task(name="app.tasks.fetch_matches_in_background")
 def fetch_matches_in_background(league):
-    """
-    Fetch football matches in the background and cache them.
-    """
-    from app.routes import LEAGUES  # Local import to avoid circular dependencies
-    url = LEAGUES.get(league, LEAGUES['eredivisie'])
-    logger.info(f"Fetching football matches for league: {league} with URL: {url}")
-    try:
-        matches = asyncio.run(fetch_all_matches_async(url))
-        logger.info(f"Fetched football matches: {matches}")
-        cache.set(f"matches_{league}", matches, timeout=3600)
-        logger.info(f"Football data for {league} successfully cached.")
-        return matches
-    except Exception as e:
-        logger.error(f"Error fetching football data for {league}: {e}")
-        return None
+    with app.app_context():
+        from app.routes import LEAGUES
+
+        url = LEAGUES.get(league, LEAGUES['eredivisie'])
+        logger.info(f"Fetching football matches for league: {league} with URL: {url}")
+
+        try:
+            matches = fetch_all_matches_async(url)
+            cache.set(f"matches_{league}", matches, timeout=3600)
+            logger.info(f"Football data for {league} successfully cached.")
+        except Exception as e:
+            logger.error(f"Error fetching football data for {league}: {e}")
