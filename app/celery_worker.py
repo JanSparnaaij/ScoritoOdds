8139fe59eff605b8e11ece1fb5299929d066d811
Jan Sparnaaij <jcmsparnaaij@gmail.com>
2025-01-16 14:57:03 +0100

    set scripts

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -6,7 +6,6 @@ def create_celery_app(app=None):
     redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
     ssl_cert_path = os.getenv("SSL_CERT_PATH", None)
     environment = os.getenv("FLASK_ENV", "development")  # Default to development
-    print(environment)
     
     celery = Celery(
         app.import_name if app else __name__,
@@ -18,7 +17,6 @@ def create_celery_app(app=None):
     # Update configuration for SSL if using rediss://
     if redis_url.startswith("rediss://"):
         if environment == "production":
-            # Strict SSL validation for production
             ssl_config = {"ssl_cert_reqs": "required"}
             if ssl_cert_path:
                 ssl_config["ssl_ca_certs"] = ssl_cert_path
@@ -34,9 +32,18 @@ def create_celery_app(app=None):
     # Update with Flask app configuration if app is provided
     if app:
         celery.conf.update(app.config)
-        celery.conf.update({"broker_connection_retry_on_startup": True})
+
+    celery.conf.update(
+        worker_concurrency=2,
+        worker_prefetch_multiplier=1,
+        task_acks_late=True,
+        task_soft_time_limit=300,
+        task_time_limit=360,
+        broker_heartbeat=10,
+        broker_connection_retry=True,
+        broker_connection_max_retries=5,
+    )
 
     return celery
 
-# Instantiate Celery without Flask app context
 celery = create_celery_app()
